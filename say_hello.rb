91f4546986660238d6bf52374900c913b1f5fcbf
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-11 21:41:41 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,2 +1,10 @@
-def say_hello
+def say_hello(name)
+  put "Hello #{name}!"
+end
+
+
+  it 'defaults to Ruby Programmer when no name is passed in' do
+    expect($stdout).to receive(:puts).with("Hello Ruby Programmer!")
+    say_hello()
+  end
 end
